7f2169583fcef9db054771d2f0aff99be3056f6d
Jordan Ward <jordancward@gmail.com>
2024-10-22 10:58:23 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Import flask to use multiple HTML web pages
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# the next line is connected to home function because of the @ symbol
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# Special syntax around station and date
+@app.route("/api/v1/<word>")
+def about(word):
+    uppercase = word.upper()
+    temperature = 23
+    return {"definition": uppercase,
+            "word": word}
+
+
+# Only runs the website if this script is run directly from this file
+if __name__ == "__main__":
+    # Adding debug=True allows us to see errors on the web page
+    app.run(debug=True)
